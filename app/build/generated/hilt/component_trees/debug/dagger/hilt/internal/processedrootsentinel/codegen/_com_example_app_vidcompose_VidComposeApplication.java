82b7d1072bc1a86517603fefa7e6f36986e3d878
Azro08 <azro.skib08@gmail.com>
2024-01-27 22:50:51 +0300

    clean project

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_app_vidcompose_VidComposeApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.app.vidcompose.VidComposeApplication"
-)
-public final class _com_example_app_vidcompose_VidComposeApplication {
-}
